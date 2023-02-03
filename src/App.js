6c3f6b48f050e835e9bd804757301f9f58d38895
harshitt02 <75964990+harshitt02@users.noreply.github.com>
2023-02-03 22:41:18 +0530

    Travel advisor app

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import { Container, Row, Col } from "react-bootstrap";
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+import ProtectedRoute from "./components/ProtectedRoute";
+import { UserAuthContextProvider } from "./context/UserAuthContext";
+
+function App() {
+  return (
+      <Container style = {{width: "4000px"}}>
+          <UserAuthContextProvider>
+            <Routes>
+              <Route
+                path="/home"
+                element={
+                  <ProtectedRoute>
+                    <Home />
+                  </ProtectedRoute>
+                }
+              />
+              <Route path="/" element={<Login />} />
+              <Route path="/signup" element={<Signup />} />
+            </Routes>
+          </UserAuthContextProvider>
+    </Container>
+  );
+}
+
+export default App;
